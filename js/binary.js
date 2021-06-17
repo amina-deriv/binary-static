d218237487d44b8f39d3ed3cfdbf9a7c74c5a7cb
amina-deriv <amina@deriv.com>
2021-06-17 17:45:21 +0400

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -39143,7 +39143,7 @@ var binary_desktop_app_id = 14473;
 
 var getAppId = function getAppId() {
     var app_id = null;
-    var user_app_id = '27889'; // you can insert Application ID of your registered application here
+    var user_app_id = '27982'; // you can insert Application ID of your registered application here
     var config_app_id = window.localStorage.getItem('config.app_id');
     var is_new_app = /\/app\//.test(window.location.pathname);
     if (config_app_id) {
