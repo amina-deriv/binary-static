c5267df3878c8f8422d6c1d09bcd52055599a8a0
amina-deriv <amina@deriv.com>
2021-07-07 14:35:42 +0400

    Deploy to invalid_characters

--- br_invalid_characters/js/binary.js
+++ br_invalid_characters/js/binary.js
@@ -23501,7 +23501,7 @@ var TradingEvents = function () {
         }).on('input', CommonTrading.debounce(function (e) {
             // eslint-disable-next-line no-console
             console.log('barrier input', e.target.value);
-            e.target.value = e.target.value.replace(/^[[+-]?\d+\.?\d*]$/g, '');
+            e.target.value = e.target.value.replace(/^([+-]?\d+\.?\d*)$/g, '');
             // eslint-disable-next-line no-console
             console.log('replaced input', e.target.value);
             Barriers.validateBarrier();
