22298dd86880ba59c11c54df8d83fd79db198c16
amina-deriv <amina@deriv.com>
2021-10-12 09:17:45 +0400

    lint fix

--- src/javascript/app/pages/cashier/payments_page/payments_data.js
+++ src/javascript/app/pages/cashier/payments_page/payments_data.js
@@ -34,26 +34,27 @@ const showPaymentData = () => {
 
     BinarySocket.wait('website_status', 'authorize', 'landing_company').then(() => {
         current_client_country = Client.get('residence') || State.getResponse('website_status.clients_country');
+        // eslint-disable-next-line no-console
         console.log(current_client_country);
-            payment_method_json.map(item => {
-                const showItem = filterItem(item, current_client_country);
-                if (!showItem) {
-                    $(`tr[data-anchor='${item.key}']`).remove();
-                    $('#payment_methods > div').each(function () {
-                        const rowContents = $(this).find('tbody').html();
-                        if (!rowContents) {
-                            $(this).remove();
-                        }
-                    });
-                }
-                $('#payment_methods_loading').remove();
-              
-                if (!$('#payment_methods').children().length){
-                    $('#no_payment_methods').setVisibility(1);
-                } else {
-                    $('#payment_methods').setVisibility(1);
-                    $('#payments_footer').setVisibility(1);
-                }
+        payment_method_json.map(item => {
+            const showItem = filterItem(item, current_client_country);
+            if (!showItem) {
+                $(`tr[data-anchor='${item.key}']`).remove();
+                $('#payment_methods > div').each(function () {
+                    const rowContents = $(this).find('tbody').html();
+                    if (!rowContents) {
+                        $(this).remove();
+                    }
+                });
+            }
+            $('#payment_methods_loading').remove();
+
+            if (!$('#payment_methods').children().length) {
+                $('#no_payment_methods').setVisibility(1);
+            } else {
+                $('#payment_methods').setVisibility(1);
+                $('#payments_footer').setVisibility(1);
+            }
         }
         );
     });
