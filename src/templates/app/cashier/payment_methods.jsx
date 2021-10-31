a1cb6d28521fc183d8f38c3d0bbf12e23dcfd193
amina-deriv <amina@deriv.com>
2021-10-31 10:08:12 +0400

    debug

--- src/templates/app/cashier/payment_methods.jsx
+++ src/templates/app/cashier/payment_methods.jsx
@@ -26,8 +26,6 @@ const TableValues = ({ value }) => {
 
 const PaymentLogo = ({ logo, name }) => {
     const logoFilePath = `src/images/pages/payment_methods/${logo}.svg`;
-    // eslint-disable-next-line no-console
-    console.log(logoFilePath,fs.existsSync(logoFilePath));
     if (!fs.existsSync(logoFilePath)) {
         return <div className='payment-methods__noIconText'>{`${name}`}</div>;
     }
