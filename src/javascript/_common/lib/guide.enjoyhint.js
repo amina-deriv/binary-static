375677fa4dd7e47036d667155cf0475c24544517
amina-deriv <amina@deriv.com>
2021-12-21 17:22:54 +0400

    condition to check guidecomponent

--- src/javascript/_common/lib/guide.enjoyhint.js
+++ src/javascript/_common/lib/guide.enjoyhint.js
@@ -1033,7 +1033,8 @@ $.fn.enjoyhint = function (method) {
 };
 
 $(window).on('popstate', function() {
-    var $body = $('body');
-    $body.enjoyhint('hide');
+    if (document.getElementById('guideBtn')) {
+        var $body = $('body');
+        $body.enjoyhint('hide')
+    }
 });
-
